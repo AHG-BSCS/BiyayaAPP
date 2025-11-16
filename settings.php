49b4336724f1cc92f17de320fa982623b5c6e521
Francis Constantino <155714369+francisconstantino1@users.noreply.github.com>
2025-11-16 09:11:46 +0800

    remove one role

--- settings.php
+++ settings.php
@@ -1930,7 +1930,6 @@ $church_logo = getChurchLogo($conn);
                     <label for="new_role">Role</label>
                     <select id="new_role" name="new_role" class="form-control" required>
                         <option value="Member">Member</option>
-                        <option value="Pastor">Pastor</option>
                         <option value="Administrator">Administrator</option>
                         <option value="Super Admin">Super Admin</option>
                     </select>
